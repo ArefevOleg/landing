27a3df66fd010b1bfe4ba08e9bf429cc9a0aaadd
Arefevpro <arefevmsk@gmail.com>
2024-07-19 18:46:41 +0300

    ./

---
+++ src/styles/Theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+    colors: {
+        lightBg: "#ffffff",
+        darkBg: "rgba(23, 23, 23, 1)",
+        accent: "#d70e33",
+    },
+
+    fontColor: {
+        mainColor: "rgba(250, 250, 249, 1)",
+    },
+
+    btnColors: {
+        bgColorBlue: "rgba(0, 112, 201, 1)",
+        bgColorRed: "rgba(230, 83, 60, 1)",
+    },
+
+    cardColors: {
+        colorAverage: "rgba(50, 52, 67, 1)",
+        colorLight: "rgba(35, 37, 50, 1)",
+        colorDark: "rgba(79, 82, 97, 1)",
+    },
+
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
+        desktop: "screen and (min-width: 1140px)",
+    }
+}
